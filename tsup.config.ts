2fef6692e78cbb6f969a8e5183f2cbf4121729c1
HeFung <HeFung@users.noreply.github.com>
2024-08-01 20:12:09 +0800

    deploy: viptv-work/dev@97047b80b1fefa04b935464010d3dc5791e1de98

--- tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "tsup";
-
-export const tsup = defineConfig({
-  entry: ["src/main.ts"],
-  outDir: "lib",
-  clean: true,
-  format: ["esm"],
-  minify: process.env.NODE_ENV !== "development",
-});
